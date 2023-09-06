cd5c93fe60ac609606708c33f97ba1bf44429863
kavitagashni <kavitagashni@gmail.com>
2023-09-06 20:55:05 +0530

    add new file

--- repeatString.js
+++ repeatString.js
@@ -1,22 +1,22 @@
 function repeat(str, num) {
-    var output ="";
-    if(num>0){
-    for (var i = 0; i < num; i++) {
-        output =output + str;
-    
+    var output = "";
+    if (num > 0) {
+        for (var i = 0; i < num; i++) {
+            output = output + str;
 
-}
-console.log(output);
-}
-else{
-    console.log("Error");
-}
+
+        }
+        console.log(output);
+    }
+    else {
+        console.log("Error");
+    }
 }
 
-repeat("Shivi",4)
-repeat("ping",1)
-repeat("ping",0)
-repeat("hey",-2)
+repeat("Shivi", 4)
+repeat("ping", 1)
+repeat("ping", 0)
+repeat("hey", -2)
 
 
 
